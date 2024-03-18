74040d5f590dd3deb6ba74c6ec4da3bff87ad38c
Hady Mohammed <hady.m.meawad@gmail.com>
2024-03-18 14:28:12 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 /*
- * File: CS112_A2_T3_SectionNum_20230455_20230382_20230603.cpp
+ * File:        CS112_A2_T3_SectionNum_20230455_20230382_20230603.cpp
  *
- * Purpose: Fraction Calculator (Summation, Subtraction, Multiplication and Division)
+ * Purpose:     Fraction Calculator (Summation, Subtraction, Multiplication and Division)
  *
- * Author:      Hady Mohammed Meawad Mohammed       20230455        meawad.hady@gmail.com
- *              Marwan Hussein Mohamed Mohamed      20230382        marawanlabana649@gmail.com
- *              Mahmoud Abdelaziz Mahmoud           20230603        20230603@stud.fci-cu.edu.eg
+ * Author:      Hady Mohammed Meawad Mohammed           20230455        meawad.hady@gmail.com
+ *              Marwan Hussein Mohamed Mohamed          20230382        marawanlabana649@gmail.com
+ *              Mahmoud Abdelaziz Mahmoud               20230603        20230603@stud.fci-cu.edu.eg
  *
  * Lab Number:  S19, S20
  * TA:          Eng. Yomna Fathy
@@ -14,13 +14,11 @@
  *              20230382:   marawanlabana649@gmail.com
  *              20230603:   20230603@stud.fci-cu.edu.eg
  *
+ * ID1:         20230455 – Input Validation, Program Switch Case, Simplification Function, and Multiplication
  *
- * ID1: 20230455 – Input Validation, Program Switch Case, Simplification Function, Division and Multiplication
+ * ID2:         20230382 – Improper Fraction Validation, Subtraction, Summation
  *
- * ID2: 20230382 – Improper Fraction Validation, Subtraction, Summation
- *
- * ID3: 20230603 – the part s/he did
- *
+ * ID3:         20230603 – Division function, Algorithm and Pseudocode
 */
 
 
@@ -60,6 +58,14 @@ void simplify(int c[2]) {
 }
 
 
+/*
+ * Fractions Format Used in Calculations
+ * a[0]: first numerator
+ * a[1]: first denominator
+ * b[0]: second numerator
+ * b[1]: second denominator
+*/
+
 void multiply(int a[2], int b[2]) {
     int c[2];
     c[0] = a[0] * b[0];
@@ -92,39 +98,65 @@ void sub(int a[2], int b[2]) {
 
 
 int main() {
+    // Declare variables for two fractions, a symbol, and input strings
     int f1[2], f2[2];
     char symbol;
     string fraction1, fraction2;
 
+    // Display welcome message
+    cout << "Welcome to the Fractions Calculator." << endl;
+
+    // Main loop for calculator operations
     while (true) {
-        string input_ = "", input = "";
+        string input_ = "", input = ""; // Reset input strings each iteration
+
+        // Prompt user for input fraction format
+        cout << "Expressions must be written like this: a/b [-+*/] c/d" << endl;
         cout << "Enter fractions to calculate or exit:";
         getline(cin, input_);
 
+        // Convert input to lowercase for case insensitivity
         for (char x: input_) {
             input += tolower(x);
         }
 
+        // Check if user wants to exit
         if (input == "exit") {
             cout << "Thank you for using our Fraction Calculator" << endl;
             break;
         }
 
+        // Find space to separate fractions and symbol
         int f_space = input.find(" ");
         fraction1 = input.substr(0, f_space);
         f_space = input.find(" ", f_space + 1);
         symbol = input[f_space - 1];
         fraction2 = input.substr(f_space + 1);
 
-        string symbols = "+-/*";
 
-
+        // Ensure fractions have denominators
         if (fraction1.find('/') == string::npos)
             fraction1 += "/1";
         if (fraction2.find('/') == string::npos)
             fraction2 += "/1";
 
+        // Declaring mathematical Symbols
+        string symbols = "+-/*";
+
+        // Using regex check for fraction patterns in each string to Validate the format of input fractions using regular expressions
+        if (!regex_match(fraction1, regex("[-+]?[0-9]+/[-+]?[1-9]+"))) {        // Check first fraction format
+            cout << "Invalid operation. Try again" << endl;
+            continue;
+        } else if (!regex_match(fraction2, regex("[-+]?[0-9]+/[-+]?[1-9]+"))) { // Check Second fraction format
+            cout << "Invalid operation. Try again" << endl;
+            continue;
+        } else if (symbols.find(symbol) == string::npos) {      // if entered symbol isn't valid, Re-prompt user to enter the expression correctly
+            cout << "Invalid operand" << endl;
+            continue;
+        }
+
         // convert strings to fraction, each fraction in an array, where f1[0] is the nominator and f1[1] is the denominator.
+        // Dividing the string based on "/" position
         f1[0] = stoi(fraction1.substr(0, fraction1.find('/')));
         f1[1] = stoi(fraction1.substr(fraction1.find('/') + 1));
         f2[0] = stoi(fraction2.substr(0, fraction2.find('/')));
@@ -132,44 +164,27 @@ int main() {
 
 
 
-        // using regex check for fraction patterns in each string
-        if (!regex_match(fraction1, regex("[-+]?[0-9]+/[-+]?[1-9]+"))) {
-            cout << "Invalid operation. Try again" << endl;
-            continue;
-        } else if (!regex_match(fraction2, regex("[-+]?[0-9]+/[-+]?[1-9]+"))) {
-            cout << "Invalid operation. Try again" << endl;
-            continue;
-        } else if (symbols.find(symbol) == string::npos) {
-            cout << "Invalid operand" << endl;
-            continue;
-        }
-
-
-
-
-
-        // according to calculation symbol, switch to function
+        // Perform the appropriate calculation based on the operation symbol using a switch statement
         switch (symbol) {
             case '/': {
                 divide(f1, f2);
-                continue;
+                continue;   // Continue to next iteration
             }
             case '*': {
                 multiply(f1, f2);
-                continue;
+                continue;   // Continue to next iteration
             }
             case '+': {
                 sum(f1, f2);
-                continue;
+                continue;   // Continue to next iteration
             }
             case '-': {
                 sub(f1, f2);
-                continue;
+                continue;   // Continue to next iteration
             }
         }
 
     }
-
 
     return 0;
 }
